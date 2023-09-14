ddccf183bc526519d28ee65a1e22a444d7135c11
aaron1986 <aaronbrucesmith86"gmail.com>
2023-09-14 13:03:50 +0100

    Code for Video

---
+++ ~/.vscode-root/User/History/511d3626/XSik.js
@@ -0,0 +1,11 @@
+function RickAshley() {
+    let repsonse = prompt("Do you like Rick Ashley");
+    let message = "I have given you up!";
+
+    if(repsonse || repsonse.toLowerCase === "yes") {
+        window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ/embed/VIDEO_ID?autoplay=1";
+    } else if (repsonse || repsonse.toLowerCase === "no") {
+        document.write(message); 
+    }
+    
+} 
--- ~/.vscode-root/User/History/511d3626/lcJv.js
+++ ~/.vscode-root/User/History/511d3626/lcJv.js
@@ -2,7 +2,7 @@ function RickAshley() {
     let repsonse = prompt("Do you like Rick Ashley");
     let message = "I have given you up!";
 
-    if(repsonse ==="yes") {
+    if(repsonse || repsonse.toLowerCase === "yes") {
         window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ/embed/VIDEO_ID?autoplay=1";
     } else if (repsonse === "no") {
         document.write(message); 
